5359f77a05649bd5c7f38a009a15fed3a6c4a727
Emon <emon.mhk69@gmail.com>
2023-12-02 21:20:18 +0600

    mongo db connection done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -10,11 +11,35 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/", (req,res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyyhzcl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
     res.send("Edu-Care Soltuion network is running...");
 })
 
 
-app.listen(port, (req,res)=>{
+app.listen(port, (req, res) => {
     console.log(`Edu-Care Soltuion API is running on port: ${port}`);
 })
