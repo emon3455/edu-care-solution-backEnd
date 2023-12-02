a13544526b0c4bee144eb5b89b2019aab4ffe39c
Emon <emon.mhk69@gmail.com>
2023-12-03 00:44:06 +0600

    all get operation done for each end point

--- index.js
+++ index.js
@@ -24,6 +24,55 @@ async function run() {
     try {
         await client.connect();
 
+        // collections:
+        const classesCollection = client.db("eduCareSolutions").collection("classes");
+        const blogsCollection = client.db("eduCareSolutions").collection("blogs");
+        const sessionsCollection = client.db("eduCareSolutions").collection("sessions");
+        const usersCollection = client.db("eduCareSolutions").collection("users");
+
+        // classes:
+        app.get("/classes", async(req, res)=>{
+            const query={}
+            if(req.query.email){
+                query = {email: req.query.email}
+            }
+            const result = await classesCollection.find(query).toArray();
+            res.send(result)
+        })
+
+
+        // sessions:
+        app.get("/sessions", async(req, res)=>{
+            const query={}
+            if(req.query.email){
+                query = {email: req.query.email}
+            }
+            const result = await sessionsCollection.find(query).toArray();
+            res.send(result)
+        })
+
+
+        // blogs:
+        app.get("/blogs", async(req, res)=>{
+            const query={}
+            if(req.query.email){
+                query = {email: req.query.email}
+            }
+            const result = await blogsCollection.find(query).toArray();
+            res.send(result)
+        })
+
+
+        // users:
+        app.get("/users", async(req, res)=>{
+            const query={}
+            if(req.query.email){
+                query = {email: req.query.email}
+            }
+            const result = await usersCollection.find(query).toArray();
+            res.send(result)
+        })
+
 
 
         await client.db("admin").command({ ping: 1 });
