63c003a7f3b8ce3ff2fa7dc015292e6591d79058
Emon <emon.mhk69@gmail.com>
2023-12-15 02:22:30 +0600

    v1 deploy done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         // collections:
         const categoryCollection = client.db("eduCareSolutions").collection("categorys");
@@ -265,8 +265,8 @@ async function run() {
         })
 
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
     }
