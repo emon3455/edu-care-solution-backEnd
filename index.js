0f9f4475f9a4d29a6e475f2770a2316487713ecb
Emon <emon.mhk69@gmail.com>
2023-12-15 05:34:09 +0600

    jwt token setup done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require("dotenv").config();
@@ -9,6 +10,24 @@ const port = process.env.PORT || 5000;
 // middle ware:
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unAuthorized User" });
+    }
+    const token = authorization.split(" ")[1];
+
+    // verifying:
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: true, message: "unAuthorized User" });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyyhzcl.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,7 +41,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // await client.connect();
+        await client.connect();
 
         // collections:
         const categoryCollection = client.db("eduCareSolutions").collection("categorys");
@@ -31,112 +50,123 @@ async function run() {
         const sessionsCollection = client.db("eduCareSolutions").collection("sessions");
         const usersCollection = client.db("eduCareSolutions").collection("users");
 
+
+        // -----authentication-------
+        app.post("/jwt", (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+        })
+
+
         // -----------category--------------:
-        app.get("/categorys", async(req, res)=>{
+        app.get("/categorys", async (req, res) => {
             const result = await categoryCollection.find().toArray();
             res.send(result)
         })
 
-        app.get("/categorys/:id", async(req, res)=>{
+        app.get("/categorys/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await categoryCollection.findOne(query);
             res.send(result)
         })
 
-        app.post("/categorys", async(req, res)=>{
+        app.post("/categorys", async (req, res) => {
             const category = req.body;
             const result = await categoryCollection.insertOne(category);
             res.send(result)
         })
 
-        app.patch("/categorys/:id", async(req, res)=>{
+        app.patch("/categorys/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const category = req.body;
             const updatedDoc = {
-                $set:{
+                $set: {
                     categoryName: category.categoryName
                 }
             }
-            const result = await categoryCollection.updateOne(query,updatedDoc);
+            const result = await categoryCollection.updateOne(query, updatedDoc);
             res.send(result)
         })
 
-        app.delete("/categorys/:id", async(req, res)=>{
+        app.delete("/categorys/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await categoryCollection.deleteOne(query);
             res.send(result)
         })
 
         // -----------classes---------------:
-        app.get("/classes", async(req, res)=>{
-            const query={}
-            if(req.query.email){
-                query = {email: req.query.email}
+        app.get("/classes", async (req, res) => {
+            const query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
             }
             const result = await classesCollection.find(query).toArray();
             res.send(result)
         })
 
-        app.get("/classes/:id", async(req, res)=>{
+        app.get("/classes/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await classesCollection.findOne(query);
             res.send(result)
         })
 
-        app.post("/classes", async(req, res)=>{
+        app.post("/classes", async (req, res) => {
             const classes = req.body;
             const result = await classesCollection.insertOne(classes);
             res.send(result)
         })
 
-        app.delete("/classes/:id", async(req, res)=>{
+        app.delete("/classes/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await classesCollection.deleteOne(query);
             res.send(result)
         })
 
 
         // ------------------sessions-------------------:
-        app.get("/sessions", async(req, res)=>{
-            const query={}
-            if(req.query.email){
-                query = {email: req.query.email}
+        app.get("/sessions", async (req, res) => {
+            const query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
             }
             const result = await sessionsCollection.find(query).toArray();
             res.send(result)
         })
 
-        app.get("/sessions/:id", async(req, res)=>{
+        app.get("/sessions/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await sessionsCollection.findOne(query);
             res.send(result)
         })
 
-        app.post("/sessions", async(req, res)=>{
+        app.post("/sessions", async (req, res) => {
             const sessions = req.body;
             const result = await sessionsCollection.insertOne(sessions);
             res.send(result)
         })
 
-        app.delete("/sessions/:id", async(req, res)=>{
+        app.delete("/sessions/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await sessionsCollection.deleteOne(query);
             res.send(result)
         })
 
-        app.patch("/sessions/:id", async(req, res)=>{
+        app.patch("/sessions/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const session = req.body;
             const updatedDoc = {
-                $set:{
+                $set: {
                     title: session.title,
                     sessionBanner: session.sessionBanner,
                     sessionDate: session.sessionDate,
@@ -151,59 +181,59 @@ async function run() {
 
 
         // ------------------blogs---------------------:
-        app.get("/blogs", async(req, res)=>{
-            const query={}
-            if(req.query.email){
-                query = {email: req.query.email}
+        app.get("/blogs", async (req, res) => {
+            const query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
             }
             const result = await blogsCollection.find(query).toArray();
             res.send(result)
         })
 
-        app.get("/blogs/:id", async(req, res)=>{
+        app.get("/blogs/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await blogsCollection.findOne(query);
             res.send(result)
         })
 
-        app.post("/blogs", async(req, res)=>{
+        app.post("/blogs", async (req, res) => {
             const blogs = req.body;
             const result = await blogsCollection.insertOne(blogs);
             res.send(result)
         })
 
-        app.delete("/blogs/:id", async(req, res)=>{
+        app.delete("/blogs/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await blogsCollection.deleteOne(query);
             res.send(result)
         })
 
-        app.patch("/blogs/:id", async(req, res)=>{
+        app.patch("/blogs/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const blog = req.body;
             const updatedDoc = {
-                $set:{
+                $set: {
                     title: blog.title,
                     blogBanner: blog.blogBanner,
                     description: blog.description,
                 }
             }
-            const result = await blogsCollection.updateOne(query,updatedDoc);
+            const result = await blogsCollection.updateOne(query, updatedDoc);
             res.send(result)
         })
 
-        app.patch("/blogsLike/:id", async(req, res)=>{
+        app.patch("/blogsLike/:id", async (req, res) => {
             const id = req?.params?.id;
-            const filter = {_id : new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             let like;
-            if(req.query.NoOfLike){
-                like = {NoOfLike: req.query.NoOfLike}
+            if (req.query.NoOfLike) {
+                like = { NoOfLike: req.query.NoOfLike }
             }
             const updatedDoc = {
-                $set:{
+                $set: {
                     NoOfLike: like,
                 }
             }
@@ -212,41 +242,48 @@ async function run() {
         })
 
         // ------------------users------------------:
-        app.get("/users", async(req, res)=>{
-            const query={}
-            if(req.query.email){
-                query = {email: req.query.email}
+        app.get("/users", async (req, res) => {
+            const query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
             }
             const result = await usersCollection.find(query).toArray();
             res.send(result)
         })
 
-        app.get("/users/:id", async(req, res)=>{
+        app.get("/users/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await usersCollection.findOne(query);
             res.send(result)
         })
 
-        app.post("/users", async(req, res)=>{
+        app.get("/users/role/:email", async (req, res) => {
+            const email = req?.params?.email;
+            const query = { email: email }
+            const result = await usersCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.post("/users", async (req, res) => {
             const users = req.body;
             const result = await usersCollection.insertOne(users);
             res.send(result)
         })
 
-        app.delete("/users/:id", async(req, res)=>{
+        app.delete("/users/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query);
             res.send(result)
         })
 
-        app.patch("/users/:id", async(req, res)=>{
+        app.patch("/users/:id", async (req, res) => {
             const id = req?.params?.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const user = req.body;
             const updatedDoc = {
-                $set:{
+                $set: {
                     name: user.name,
                     speciality: user.speciality,
                     phone: user.phone,
@@ -265,8 +302,8 @@ async function run() {
         })
 
 
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
     }
